e1de9d5cf41d7f84231aa20e0a4b64a697b5297f
bhargaviponnur <bhargaviponnuru68@gmail.com>
2023-11-04 12:33:09 +0530

    bhargavi

---
+++ pic.h
@@ -0,0 +1,22 @@
+//c program to print integer data type.
+#include <stdio.h>
+int main()
+{
+    char a = 'a';
+    char c;
+    printf("value of a:%c\n", a);
+    a++;
+    printf("value of a after increment is :%c\n",a)
+    // c is assigned ASCII values
+    //which corresponds to the 
+    // character 'c'
+    // a-->97 b--98 c--99
+    // here c will be printer=d
+    c=99;
+    printf ("value of c:%c",c);
+    return 0;
+        `   `
+    
+    
+
+    
